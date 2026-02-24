723a82c6947a6493d23b31588ca6b4f8cc179cb7
Aakash <aakashdahiya167@gmail.com>
2026-02-24 22:16:34 +0530

    Initial clean commit

--- config/firebase.js
+++ config/firebase.js
@@ -1,22 +1,17 @@
 // backend/config/firebase.js
 
 import admin from "firebase-admin";
-import fs from "fs";
-import path from "path";
-import { fileURLToPath } from "url";
 
-// Fix __dirname in ES module
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+};
 
-// Read service account manually
-const serviceAccountPath = path.join(__dirname, "serviceAccountKey.json");
-const serviceAccount = JSON.parse(
-  fs.readFileSync(serviceAccountPath, "utf-8")
-);
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
 
 export default admin;
